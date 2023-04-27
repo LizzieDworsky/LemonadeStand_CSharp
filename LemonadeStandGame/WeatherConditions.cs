76e9a490f695c616ce1f10d2723279a7ea395491
LizzieDworsky <amydworsky15@gmail.com>
2023-04-27 17:37:29 -0600

    created WeatherConditions static class

---
+++ LemonadeStandGame/WeatherConditions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStandGame
+{
+    internal static class WeatherConditions
+    {
+        private static Random rand = new Random();
+
+        public static int GetTemperature()
+        {
+            
+            return rand.Next(60, 93);
+        }
+
+        public static string GetConditions()
+        {
+            List<string> conditions = new List<string>
+            {
+                "sunny",
+                "cloudy",
+                "windy",
+                "sunny",
+                "rainy",
+                "stormy",
+                "sunny"
+            };
+            int index = rand.Next(conditions.Count);
+            return conditions[index];
+        }
+    }
+}
