b4f87f1cf3af469380fea2ce574008c554b260f1
LizzieDworsky <amydworsky15@gmail.com>
2023-04-27 18:12:29 -0600

    added Player class and assigned member variables

---
+++ LemonadeStandGame/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStandGame
+{
+    internal class Player
+    {
+        public double money;
+        public int numberLemons;
+        public int numberSugar;
+        public int numberCups;
+        public int numberIce;
+
+        public Player()
+        {
+            money = 45;
+            numberLemons = 0; 
+            numberSugar = 0; 
+            numberCups = 0;
+            numberIce = 0;
+        }
+
+        public void buyProduct()
+        {
+
+        }
+    }
+}
